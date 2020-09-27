39c63c168fce26f196a2487508641433a10130b0
T-Ralph <olaniyitralph@gmail.com>
2020-09-27 13:01:05 -0600

    Updates

---
+++ precache-manifest.e99fc1c07dc2b712b43610a4d6e0f011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad16ac543ef4f31bc066e114897f747",
+    "url": "/javascript-calculator-react-router-redux/index.html"
+  },
+  {
+    "revision": "b06acb74342a8fd8d376",
+    "url": "/javascript-calculator-react-router-redux/static/css/main.6871bd4e.chunk.css"
+  },
+  {
+    "revision": "c2abe2b3e16637e9ddd8",
+    "url": "/javascript-calculator-react-router-redux/static/js/2.89de6af6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/javascript-calculator-react-router-redux/static/js/2.89de6af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06acb74342a8fd8d376",
+    "url": "/javascript-calculator-react-router-redux/static/js/main.64eef53b.chunk.js"
+  },
+  {
+    "revision": "aa7c689d06cda9cf8a17",
+    "url": "/javascript-calculator-react-router-redux/static/js/runtime-main.7a883bcd.js"
+  },
+  {
+    "revision": "bd283081304060b20f1810e1a820e75c",
+    "url": "/javascript-calculator-react-router-redux/static/media/background.bd283081.jpg"
+  }
+]);
